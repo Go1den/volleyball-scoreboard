bbf215235ced7f8a5ccea58a6e4cff7788b7da59
Go1den <gooneden@gmail.com>
2023-04-21 10:57:12 -0500

    Tap the center of the screen 3 times to reset scores

--- js/volleyball.js
+++ js/volleyball.js
@@ -1,3 +1,5 @@
+let consecutiveMiddleTaps = 0;
+
 function changeScore(id, amount) {
     let scoreElem = document.getElementById(id);
     let score = scoreElem.innerHTML;
@@ -5,22 +7,31 @@ function changeScore(id, amount) {
     if (newScore >= 0) {
         scoreElem.innerHTML = Number(score) + amount;
     }
+    consecutiveMiddleTaps = 0;
 }
 
 function resetScores() {
     document.getElementById("awayScore").innerHTML = 0;
     document.getElementById("homeScore").innerHTML = 0;
+    consecutiveMiddleTaps = 0;
 }
 
 function onClicky(e) {
     console.log(e);
-    let middle = window.innerHeight / 2;
-    let give = Math.floor(window.innerHeight * .15);
-    if (e.clientY > middle + give) {
+    let middleY = window.innerHeight / 2;
+    let giveY = Math.floor(window.innerHeight * .15);
+    if (e.clientY > middleY + giveY) {
         changeScore(e.srcElement.id, -1);
-    } else if (e.clientY < middle - give) {
+    } else if (e.clientY < middleY - giveY) {
         changeScore(e.srcElement.id, 1);
     } else {
-        console.log("dead zone");
+        let middleX = window.innerWidth / 2;
+        let giveX = Math.floor(window.innerWidth * .1);
+        if (Math.abs(e.clientX - middleX) < giveX) {
+            consecutiveMiddleTaps++;
+            if (consecutiveMiddleTaps == 3) {
+                resetScores();
+            }
+        }
     }
 }
